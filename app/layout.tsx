b9b506a91820c1246f6c8ed94b6df7a16334b7d1
coding1100 <coding1343453@gmail.com>
2026-01-23 20:23:36 +0500

    desktop speed finalized 03

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,8 +45,7 @@ export default function RootLayout({
         />
 
 
-        {/* DNS prefetch for resources loaded later */}
-        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
+     
         
       </head>
       <body className={poppins.className} suppressHydrationWarning={true}>
@@ -59,22 +58,7 @@ export default function RootLayout({
 
         
 
-        {/* GTM - use lazyOnload instead of default to reduce main thread work */}
-        <Script
-          id="gtm-script"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5ZHV46X');
-            `,
-          }}
-        />
-
-
+        
         {/* Client-side scripts that need pathname */}
         <ClientScripts />
       </body>
