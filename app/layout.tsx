8cced859e346df5a84d50c9281431e9bbdfebb33
coding1100 <coding1343453@gmail.com>
2026-01-26 12:39:24 +0500

    desktop speed finalized 09

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 
 
       </head>
-      <body suppressHydrationWarning={true}>
+      <body>
         <main id="main-content">{children}</main>
 
         {/* 
